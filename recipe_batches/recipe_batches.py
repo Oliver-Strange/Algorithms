92934dd25209bfbcaa546186bbab47636b944ed8
Oliver-Strange <preston.burton@gmail.com>
2019-09-12 13:35:10 -0700

    adds pseudo code and implements it

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,25 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    batches = None
+
+    for key, value in recipe.items():
+        print(key, value)
+        if key not in ingredients.keys():
+            return 0
+        make = ingredients[key] // value
+        if batches is None or make < batches:
+            batches = make
+    print(batches, "batches")
+    return batches
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
